7c716d96ae4b68936a8793e800239e2fcecb845e
Geovane Pacheco <geovane.pacheco99@gmail.com>
2017-02-27 17:29:56 -0300

    Used the tokenize to remove ?,! ' '. Finished the text classifier

--- machineLearn/text_classifier.py
+++ machineLearn/text_classifier.py
@@ -1,10 +1,12 @@
 # -*- coding: UTF-8 -*-
-
 import pandas as pd
 import numpy as np
 import nltk
-# nltk.download('stopwords') # If I want download the stio wiords
+# Downloads of auxiliary text manipulation libraries
+# nltk.download('stopwords')
 # nltk.download('rslp')
+# nltk.download('punkt')
+
 from sklearn.svm import LinearSVC
 from sklearn.cross_validation import cross_val_score
 from sklearn.multiclass import OneVsRestClassifier
@@ -26,7 +28,7 @@ def vectorize_text(text, dictionary):
 
 def fit_and_predict(model_name, model, training_data, training_marker, number_of_folds):
     # fit, predict and apply the folds
-    scores = cross_val_score(model, training_data, training_marker, cv=number_of_folds)
+    scores = cross_val_score(model, training_data, training_marker, cv=number_of_folds)    
     avg_success_rate = np.mean(scores)
 
     return avg_success_rate
@@ -42,11 +44,8 @@ if __name__ == '__main__':
     # Apply to lower function to not distinct case sensitive words
     texts = texts.str.lower()
 
-    # The next step is broken the texts in spaces
-    broken_texts = texts.str.split(' ')
-
-    # set(), to no repeat words (conjunto in portuguese)
-    words = set()
+    # The next step is broken the texts in spaces, ',', '?' and others characters
+    broken_texts = [nltk.tokenize.word_tokenize(text) for text in texts ]
 
     # Removing the stop words, that do not help the classifier
     stopwords = nltk.corpus.stopwords.words('portuguese')
@@ -54,13 +53,16 @@ if __name__ == '__main__':
     # RSLPS = Removedor de Sufixo da Lingua Portuguesa
     stemmer = nltk.stem.RSLPStemmer()
 
+    # set(), to no repeat words (conjunto in portuguese)
+    words = set()
+
     # For each word in broken_texts, I'll put only news into words
     for word_list in broken_texts:
-        # valid_words = [word for word in word_list if word not in stopwords and len(word) > 0]
-        # print 'Palavra valida: ', valid_words, '\n'
-        valid_words = [stemmer.stem(word) for word in word_list if word not in stopwords and len(word) > 0]
+        # Rejected stopwords and words with len < 2
+        valid_words = [stemmer.stem(word) for word in word_list if word not in stopwords and len(word) > 2]
         words.update(valid_words)
 
+    print 'QUANTIDADE DE PALAVRAS: ', len(words)
     # Mapping every word to a respective number
     tuplas = zip(words, xrange(len(words)))
 
@@ -101,3 +103,4 @@ if __name__ == '__main__':
     print results
 
     print '\n\nThe best model was: ', max(results, key=results.get), ': ', results[max(results, key=results.get)]
+    print multinomial_model
