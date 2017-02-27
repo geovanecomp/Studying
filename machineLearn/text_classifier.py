1bdf8479eb5ae1fb9d0558bce453d2b28c353fd6
Geovane Pacheco <geovane.pacheco99@gmail.com>
2017-02-27 13:27:14 -0300

    Function to count words' texts for every broken_text

--- machineLearn/text_classifier.py
+++ machineLearn/text_classifier.py
@@ -2,6 +2,14 @@
 
 import pandas as pd
 
+def vectorize_text(text, dictionary):
+    word_counter = [0] * len(dictionary)
+    for word in text:
+        index = dictionary[word]
+        word_counter[index] += 1
+
+    return word_counter
+
 if __name__ == '__main__':
 
     classifiers = pd.read_csv('emails.csv')
@@ -23,21 +31,11 @@ if __name__ == '__main__':
         words.update(word)
 
     # Mapping every word to a respective number
-    number_of_words = len(words)
-    tuplas = zip(words, xrange(number_of_words))
+    tuplas = zip(words, xrange(len(words)))
 
     # Making a dictionary
     dictionary = {word:index for word, index in tuplas}
 
-    word_counter = [0] * number_of_words
-    # word_counter = {}
+    counted_texts_vectors = [vectorize_text(text, dictionary) for text in broken_texts]
 
-    for text in broken_texts:
-        for word in text:
-            print word
-            index = dictionary[word]
-            print index
-            word_counter[index] += 1
-
-
-    print word_counter
+    print counted_texts
