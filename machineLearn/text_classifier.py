20743d3f8ec6642de965ef12a4bf8e3b23805327
Geovane Pacheco <geovane.pacheco99@gmail.com>
2017-02-27 12:34:48 -0300

    Broke the text in espaces to put it on a set to do not repeat words

--- machineLearn/text_classifier.py
+++ machineLearn/text_classifier.py
@@ -12,4 +12,18 @@ if __name__ == '__main__':
     # Apply to lower function to not distinct case sensitive words
     texts = texts.str.lower()
 
-    
+    # The next step is broken the texts in spaces
+    broken_texts = texts.str.split(' ')
+
+    # set(), to no repeat words (conjunto in portuguese)
+    dictionary = set()
+
+    # For each word in broken_texts, I'll put only news into dictionary
+    for word in broken_texts:
+        dictionary.update(word)
+
+
+    number_of_words = len(dictionary)
+
+    # Mapping every word to a respective number
+    zip(dictionary, xrange(number_of_words))
