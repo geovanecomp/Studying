4f287a22f0c541cd44b7e89f66c37a74870ed9ab
Geovane Pacheco <geovane.pacheco99@gmail.com>
2017-02-27 16:02:34 -0300

    Four models used for text classifiers. OneVsRest was the best model in this case

--- machineLearn/text_classifier.py
+++ machineLearn/text_classifier.py
@@ -1,6 +1,15 @@
 # -*- coding: UTF-8 -*-
 
 import pandas as pd
+import numpy as np
+
+from sklearn.svm import LinearSVC
+from sklearn.cross_validation import cross_val_score
+from sklearn.multiclass import OneVsRestClassifier
+from sklearn.multiclass import OneVsOneClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.ensemble import AdaBoostClassifier
+
 
 def vectorize_text(text, dictionary):
     word_counter = [0] * len(dictionary)
@@ -9,6 +18,13 @@ def vectorize_text(text, dictionary):
         word_counter[index] += 1
 
     return word_counter
+
+def fit_and_predict(model_name, model, training_data, training_marker, number_of_folds):
+    # fit, predict and apply the folds
+    scores = cross_val_score(model, training_data, training_marker, cv=number_of_folds)
+    avg_success_rate = np.mean(scores)
+
+    return avg_success_rate
 
 if __name__ == '__main__':
 
@@ -38,7 +54,7 @@ if __name__ == '__main__':
     dictionary = {word:index for word, index in tuplas}
 
     counted_texts_vectors = [vectorize_text(text, dictionary) for text in broken_texts]
-    
+
 
     # Adapting to the recognizer pattern
     X = counted_texts_vectors
@@ -47,7 +63,7 @@ if __name__ == '__main__':
     # Separating training and validation
     training_percentage = 0.8
 
-    len_training = training_percentage * len(Y)
+    len_training = int(training_percentage * len(Y))
     len_validation = len(Y) - len_training
 
     training_data = X[0:len_training]
@@ -55,3 +71,18 @@ if __name__ == '__main__':
 
     validation_data = X[len_training:]
     validation_marker = Y[len_training:]
+
+
+    results = {}
+    # Applying to OneVsRestClassifier
+    one_vs_rest_model   = OneVsRestClassifier(LinearSVC(random_state=0))
+    one_vs_one_model    = OneVsOneClassifier(LinearSVC(random_state=0))
+    multinomial_model   = MultinomialNB()
+    ada_boost_model     = AdaBoostClassifier()
+
+    results['OneVsRestClassifier']  = fit_and_predict("OneVsRestClassifier", one_vs_rest_model, training_data, training_marker, 10)
+    results['OneVsOneClassifier']   = fit_and_predict("OneVsOneClassifier", one_vs_one_model, training_data, training_marker, 10)
+    results['MultinomialNB']        = fit_and_predict("MultinomialNB", multinomial_model, training_data, training_marker, 10)
+    results['AdaBoostClassifier']   = fit_and_predict("AdaBoostClassifier", ada_boost_model, training_data, training_marker, 10)
+
+    print '\n\nThe best model was: ', max(results), ': ', results[max(results)]
