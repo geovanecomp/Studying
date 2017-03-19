b6bfa0211003c121539fc17d68a8556908bf0f53
Geovane Pacheco <geovane.pacheco99@gmail.com>
2017-03-18 23:10:43 -0300

    Permitindo o usuario selecionar a frase que deseja jogar atraves de uma requisicao assincrona com o servidor

--- javascript/smallProjects/GegeTyper-Jquery/public/js/frase.js
+++ javascript/smallProjects/GegeTyper-Jquery/public/js/frase.js
@@ -1,4 +1,6 @@
 $('#botao-frase').click(fraseAleatoria)
+$('#botao-frase-id').click(buscaFrase)
+
 
 function fraseAleatoria() {
     $('#spinner').toggle();
@@ -24,3 +26,59 @@ function trocaFraseAleatoria(data) {
     atualizaTamanhoFrase(data[posAleatoria].texto.length)
     atualizaTempoInicial(data[posAleatoria].tempo)
 }
+
+
+function buscaFrase() {
+    $('#spinner').toggle();
+    let fraseId = $('#frase-id').val()
+    console.log(fraseId);
+    // Parametros que devem ser enviados como objetos
+    let dados = {
+        id: fraseId
+    }
+    $.get('http://localhost:3000/frases', dados, trocaFrase)
+    .fail(function() {
+        $('#erro').toggle()
+        setTimeout(function() {
+            $('#erro').toggle()
+        }, 1500)
+    })
+    // Sempre executara, mesmo quando for falha ou sucesso
+    .always(function() {
+        $('#spinner').toggle();
+    })
+}
+
+
+function trocaFrase(data) {
+    $('.frase').text(data.texto)
+    atualizaTamanhoFrase(data.texto.length)
+    atualizaTempoInicial(data.tempo)
+}
+
+
+// Buscando tudo e filtrando via javascript
+// function buscaFrase() {
+//     $('#spinner').toggle();
+//     $.get('http://localhost:3000/frases', function(resposta) {
+//         let id = parseInt($('#frase-id').val())
+//
+//         for (var i = 0; i < resposta.length; i++) {
+//             console.log(resposta[i], id);
+//             if(resposta[i]._id === id){
+//                 $('.frase').text(resposta[i].texto)
+//             }
+//         }
+//
+//     })
+//     .fail(function() {
+//         $('#erro').toggle()
+//         setTimeout(function() {
+//             $('#erro').toggle()
+//         }, 1500)
+//     })
+//     // Sempre executara, mesmo quando for falha ou sucesso
+//     .always(function() {
+//         $('#spinner').toggle();
+//     })
+// }
