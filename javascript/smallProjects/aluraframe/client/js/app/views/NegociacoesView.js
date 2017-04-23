ab34de974110354ad12618a89732d6dc42572e5f
Geovane Pacheco <geovane.pacheco99@gmail.com>
2017-04-23 11:21:07 -0300

    Exibindo notificacao e cadastro com sucesso e tambem aplicando heranca entre as views

--- javascript/smallProjects/aluraframe/client/js/app/views/NegociacoesView.js
+++ javascript/smallProjects/aluraframe/client/js/app/views/NegociacoesView.js
@@ -1,6 +1,7 @@
-class NegociacoesView {
+class NegociacoesView extends View{
+
     constructor(elemento) {
-        this._elemento = elemento
+        super(elemento)
     }
 
     _template(model) {
@@ -41,25 +42,4 @@ class NegociacoesView {
         `
     }
 
-    update(model) {
-        this._elemento.innerHTML = this._template(model)
-    }
 }
-
-/*<table class="table table-hover table-bordered">
-    <thead>
-        <tr>
-            <th>DATA</th>
-            <th>QUANTIDADE</th>
-            <th>VALOR</th>
-            <th>VOLUME</th>
-        </tr>
-
-    </thead>
-    <tbody>
-
-    </tbody>
-    <tfoot>
-
-    </tfoot>
-</table>*/
