955c80e5b72ffd06c3efb3fc51a41ac69858b4ae
Geovane Pacheco <geovane.pacheco99@gmail.com>
2017-04-22 22:41:15 -0300

    Criando uma lista de negociacoes e tambem aplicando programacao defensiva

--- javascript/smallProjects/aluraframe/client/js/app/helpers/DateHelper.js
+++ javascript/smallProjects/aluraframe/client/js/app/helpers/DateHelper.js
@@ -15,7 +15,7 @@ class DateHelper {
     static textoParaData(texto) {
 
         // Validando a data
-        if(/\d{4}-\d{2}-\d{2}/.test(texto))
+        if(!/\d{4}-\d{2}-\d{2}/.test(texto))
             throw new Error('Data deve estar no formato aaaa-mm-dd')
 
 
---
+++ javascript/smallProjects/aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -0,0 +1,17 @@
+class ListaNegociacoes {
+    constructor() {
+        this._negociacoes = []
+    }
+
+    adiciona(negociacao) {
+        this._negociacoes.push(negociacao)
+    }
+
+    get negociacoes() {
+        // Programação defensiva!
+        // Criar um novo array em memoria, impossibilitando que sejam feitas
+        // alterações em meu atributo (como por exemplo via push) e sim na
+        // cópia que estou retornando.
+        return [].concat(this._negociacoes)
+    }
+}
