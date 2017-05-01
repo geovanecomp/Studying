f2d9420f66cabcb272122cd76f7f0e9238751ffb
Geovane Pacheco <geovane.pacheco99@gmail.com>
2017-05-01 13:34:09 -0300

    Service de promise para negociacoes

--- javascript/smallProjects/aluraframe/client/js/app/services/NegociacaoService.js
+++ javascript/smallProjects/aluraframe/client/js/app/services/NegociacaoService.js
@@ -1,124 +1,106 @@
 class NegociacaoService {
-    obterNegociacoesDaSemana(callback) {
-        let xhr = new XMLHttpRequest()
 
-        // xhr.open('GET', 'http://www.google.com.br')
-        xhr.open('GET', 'negociacoes/semana');
+    obterNegociacoesDaSemana() {
 
-        /* Estados de xhr
+        return new Promise((resolve, reject) => {
+
+            let xhr = new XMLHttpRequest()
+
+            xhr.open('GET', 'negociacoes/semana');
+
+            /* Estados de xhr
             0: requisicao ainda nao iniciada
             1: conexao com o servidor estabelecida
             2: requisicao recebida
             3: processando requisicao
             4: requisicao conlcuida e a resposta esta pronta
-                // Pode acontecer um erro no servidor, e ele me retornar uma resposta valida
-                //entao tambem tenho que conferir a resposta dada pelo servidor (status 200)
-        */
+            // Pode acontecer um erro no servidor, e ele me retornar uma resposta valida
+            //entao tambem tenho que conferir a resposta dada pelo servidor (status 200)
+            */
 
-        xhr.onreadystatechange = () => {
-            if(xhr.readyState == 4) {
-                if (xhr.status == 200) {
-                    console.log('Obtendo as negociacoes do servidor')
-                    //Obtendo a resposta do servidor
-                    //JSON.parse converte para objetos javascript
-                    let lista = JSON.parse(xhr.responseText)
+            xhr.onreadystatechange = () => {
+                if(xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        console.log('Obtendo as negociacoes do servidor')
+                        //Obtendo a resposta do servidor
+                        //JSON.parse converte para objetos javascript
+                        let listaNegociacoes = JSON.parse(xhr.responseText)
                         .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
 
-                    callback(null, lista)
+                        resolve(listaNegociacoes)
 
 
-                } else {
-                    //Exibindo erro retornado pelo servidor
-                    console.log(xhr.responseText)
-                    callback('Não foi possivel obter as negociações', null)
+                    } else {
+                        //Exibindo erro retornado pelo servidor
+                        console.log(xhr.responseText)
+                        reject('Não foi possivel obter as negociações')
+                    }
                 }
+
             }
 
-        }
+            xhr.send()
 
-        xhr.send()
+        })
 
     }
 
-    obterNegociacoesDaSemanaAnterior(callback) {
-        let xhr = new XMLHttpRequest()
+    obterNegociacoesDaSemanaAnterior() {
+        return new Promise((resolve, reject) => {
+            let xhr = new XMLHttpRequest()
 
-        // xhr.open('GET', 'http://www.google.com.br')
-        xhr.open('GET', 'negociacoes/anterior');
+            xhr.open('GET', 'negociacoes/anterior');
 
-        /* Estados de xhr
-            0: requisicao ainda nao iniciada
-            1: conexao com o servidor estabelecida
-            2: requisicao recebida
-            3: processando requisicao
-            4: requisicao conlcuida e a resposta esta pronta
-                // Pode acontecer um erro no servidor, e ele me retornar uma resposta valida
-                //entao tambem tenho que conferir a resposta dada pelo servidor (status 200)
-        */
+            xhr.onreadystatechange = () => {
+                if(xhr.readyState == 4) {
+                    if (xhr.status == 200) {
 
-        xhr.onreadystatechange = () => {
-            if(xhr.readyState == 4) {
-                if (xhr.status == 200) {
-                    console.log('Obtendo as negociacoes do servidor')
-                    //Obtendo a resposta do servidor
-                    //JSON.parse converte para objetos javascript
-                    let lista = JSON.parse(xhr.responseText)
-                        .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
+                        let listaNegociacoes = JSON.parse(xhr.responseText)
+                            .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
 
-                    callback(null, lista)
+                        resolve(listaNegociacoes)
 
-
-                } else {
-                    //Exibindo erro retornado pelo servidor
-                    console.log(xhr.responseText)
-                    callback('Não foi possivel obter as negociações', null)
+                    } else {
+                        console.log(xhr.responseText)
+                        reject('Não foi possivel obter as negociações')
+                    }
                 }
+
             }
 
-        }
+            xhr.send()
 
-        xhr.send()
+        })
 
     }
 
-    obterNegociacoesDaSemanaRetrasada(callback) {
-        let xhr = new XMLHttpRequest()
+    obterNegociacoesDaSemanaRetrasada() {
+        return new Promise((resolve, reject) => {
+            let xhr = new XMLHttpRequest()
 
-        // xhr.open('GET', 'http://www.google.com.br')
-        xhr.open('GET', 'negociacoes/retrasada');
+            xhr.open('GET', 'negociacoes/retrasada');
 
-        /* Estados de xhr
-            0: requisicao ainda nao iniciada
-            1: conexao com o servidor estabelecida
-            2: requisicao recebida
-            3: processando requisicao
-            4: requisicao conlcuida e a resposta esta pronta
-                // Pode acontecer um erro no servidor, e ele me retornar uma resposta valida
-                //entao tambem tenho que conferir a resposta dada pelo servidor (status 200)
-        */
+            xhr.onreadystatechange = () => {
+                if(xhr.readyState == 4) {
+                    if (xhr.status == 200) {
 
-        xhr.onreadystatechange = () => {
-            if(xhr.readyState == 4) {
-                if (xhr.status == 200) {
-                    console.log('Obtendo as negociacoes do servidor')
-                    //Obtendo a resposta do servidor
-                    //JSON.parse converte para objetos javascript
-                    let lista = JSON.parse(xhr.responseText)
-                        .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
+                        let listaNegociacoes = JSON.parse(xhr.responseText)
+                            .map(objeto => new Negociacao(new Date(objeto.data), objeto.quantidade, objeto.valor))
 
-                    callback(null, lista)
+                        resolve(listaNegociacoes)
 
 
-                } else {
-                    //Exibindo erro retornado pelo servidor
-                    console.log(xhr.responseText)
-                    callback('Não foi possivel obter as negociações', null)
+                    } else {
+                        console.log(xhr.responseText)
+                        reject('Não foi possivel obter as negociações')
+                    }
                 }
+
             }
 
-        }
+            xhr.send()
+        })
 
-        xhr.send()
 
     }
 }
