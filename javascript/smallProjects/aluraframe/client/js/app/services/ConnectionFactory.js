13339013026b7278faa54a3f33995e506a98d7be
Geovane Pacheco <geovane.pacheco99@gmail.com>
2017-05-27 12:02:08 -0300

    Criando uma connection factoru e utilizando o module pattern

---
+++ javascript/smallProjects/aluraframe/client/js/app/services/ConnectionFactory.js
@@ -0,0 +1,58 @@
+
+// Adicionando a connection factory em um módulo para não deixar seus atributos
+// de forma visivel fora do escopo. Já que esta dentro de uma funcao, as variaves
+// possuem escopo local. E retorno a classe, para assim ter acesso a classe e seus metodos
+
+// Mas ainda havia o problema da funcao 'tmp' pode ser chamada, por isso a trocou
+// por uma funcao anonima e a auto invocou envolvendo-a com parenteses.
+var ConnectionFactory = (function () {
+
+    var stores = ['negociacoes']
+    var version = 4
+    var dbName = 'aluraFrame'
+
+    var connection = null
+
+    return class ConnectionFactory {
+
+        constructor() {
+            throw new Error("Não é possível criar instanciar de ConnectionFactory")
+        }
+
+        static getConnection() {
+            return new Promise((resolve, reject) => {
+                let openRequest = window.indexedDB.open(dbName, version)
+
+                // Triade de eventos quando inicia-se uma requisicao de abertura no indexedDB
+                openRequest.onupgradeneeded = e => {
+                    ConnectionFactory._createStores(e.target.result)
+                }
+
+                openRequest.onsuccess = e => {
+                    if (! connection) {
+                        connection = e.target.result
+                    }
+
+                    resolve(connection)
+                }
+
+                openRequest.onerror = e => {
+                    console.log(e.target.error)
+                    reject(e.target.error.name)
+                }
+            })
+        }
+
+        static _createStores(connection) {
+
+            stores.forEach(store => {
+                if (connection.objectStoreNames.contains(store)) {
+                    connection.deleteObjectStore(store)
+                }
+                connection.createObjectStore(store, {autoIncrement: true})
+            })
+
+        }
+    }
+
+})()
