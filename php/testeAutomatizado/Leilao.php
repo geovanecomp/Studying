8f605f291804e68b4cf8ae239f79d936a9a2fc5d
Geovane Pacheco <geovane.pacheco99@gmail.com>
2017-06-10 15:22:17 -0300

    Implementando teste e regra para proibir 5 lances da mesma pessoa

--- php/testeAutomatizado/Leilao.php
+++ php/testeAutomatizado/Leilao.php
@@ -2,14 +2,14 @@
 	class Leilao {
 		private $descricao;
 		private $lances;
-		
+
 		function __construct($descricao) {
 			$this->descricao = $descricao;
 			$this->lances = array();
 		}
-		
+
 		public function propoe(Lance $lance) {
-			$this->lances[] = $lance;
+			return $this->lanceValido($lance) ? $this->lances[] = $lance : null;
 		}
 
 		public function getDescricao() {
@@ -18,6 +18,37 @@
 
 		public function getLances() {
 			return $this->lances;
+		}
+
+		public function getUltimoLance() {
+			$posicaoUltimoLance = count($this->lances) - 1;
+			return $this->lances[$posicaoUltimoLance];
+		}
+
+		private function lanceValido($lance){
+			$numeroMaximoLances = 4;
+
+			if ($this->contarNumeroLances($lance->getUsuario()) > $numeroMaximoLances) {
+				return false;
+			}
+
+			if (count($this->lances) > 0 && $this->getUltimoLance()->getUsuario() == $lance->getUsuario()) {
+				return false;
+			}
+
+			return true;
+		}
+
+		private function contarNumeroLances(Usuario $usuario){
+			$quantidadeLances = 0;
+
+			foreach ($this->lances as $lance) {
+				if ($lance->getUsuario() == $usuario) {
+					$quantidadeLances++;
+				}
+			}
+
+			return $quantidadeLances;
 		}
 	}
 ?>
--- php/testeAutomatizado/LeilaoTest.php
+++ php/testeAutomatizado/LeilaoTest.php
@@ -11,12 +11,62 @@ class LeilaoTest extends PHPUnit_Framework_TestCase {
 
         $geovane = new Usuario("Geovane");
 
-        $leilao->propoe(new Lance($geovane, 2000));
-
         $lanceEsperado = 2000;
+
+        $leilao->propoe(new Lance($geovane, $lanceEsperado));
 
         $this->assertEquals(1, count($leilao->getLances()));
         $this->assertEquals($lanceEsperado, $leilao->getLances()[0]->getValor());
+
+    }
+
+    public function testDeveBloquearDoisLancesSeguidos() {
+        $leilao = new Leilao("Macbook caro");
+
+        $geovane = new Usuario("Geovane");
+
+        $lanceEsperado = 2000;
+
+        $leilao->propoe(new Lance($geovane, $lanceEsperado));
+        $leilao->propoe(new Lance($geovane, 3000));
+
+        $this->assertEquals(1, count($leilao->getLances()));
+        $this->assertEquals($lanceEsperado, $leilao->getLances()[0]->getValor());
+
+    }
+
+    public function testDevePermitirAteCincoLances() {
+        $leilao = new Leilao("Macbook caro");
+
+        $geovane = new Usuario("Geovane");
+        $gege = new Usuario("Gege");
+
+        $lanceEsperado = 2000;
+
+        $leilao->propoe(new Lance($geovane, 2000));
+        $leilao->propoe(new Lance($gege, 3000));
+
+        $leilao->propoe(new Lance($geovane, 4000));
+        $leilao->propoe(new Lance($gege, 5000));
+
+        $leilao->propoe(new Lance($geovane, 6000));
+        $leilao->propoe(new Lance($gege, 7000));
+
+        $leilao->propoe(new Lance($geovane, 8000));
+        $leilao->propoe(new Lance($gege, 9000));
+
+        $leilao->propoe(new Lance($geovane, 10000));
+        $leilao->propoe(new Lance($gege, 11000));
+
+        //Deve proibir o sexto lance
+        $leilao->propoe(new Lance($geovane, 12000));
+
+        //Neste caso só pode permitir 10 lances no array
+        $this->assertEquals(10, count($leilao->getLances()));
+
+        //O ultimo lance deve ser 11000
+        $this->assertEquals(11000, $leilao->getUltimoLance()->getValor());
+
 
     }
 }
