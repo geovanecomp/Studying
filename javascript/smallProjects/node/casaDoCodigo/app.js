29bb9977e412fd870a32566b8ded7f7d73775067
Geovane Pacheco <geovane.pacheco99@gmail.com>
2017-07-12 23:10:19 -0300

    Iniciando estudos com nodejs. Bibliotecas express e nodemon instaladas, e um exemplo basico criado

---
+++ javascript/smallProjects/node/casaDoCodigo/app.js
@@ -0,0 +1,18 @@
+// Ao importar o express, ele retorna uma funcao / objeto chamada express(), que representa o express.
+let express = require('express')
+let app = express()
+
+// Usa o set quando quisermos definir variaveis para dentor o express que sera usada em todo o sistema
+// Setando qual engine sera usada
+app.set('view engine', 'ejs')
+
+app.get('/produtos', (req, res) => {
+    // res.send('<html><body>Listando os produtos da loja utilizando express!</body></html>')
+    // render(), renderizará uma página
+    console.log('zuzu2');
+    res.render('produtos/lista')
+})
+
+app.listen(3000, () => {
+    console.log('servidor rodando')
+})
