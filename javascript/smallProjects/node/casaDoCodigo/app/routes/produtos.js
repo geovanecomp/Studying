8482b6dee589340f2b7d5d2b378e317f49c4da5b
Geovane Pacheco <geovane.pacheco99@gmail.com>
2017-07-15 19:06:28 -0300

    Simulando uma classa atraves da funcao

--- javascript/smallProjects/node/casaDoCodigo/app/routes/produtos.js
+++ javascript/smallProjects/node/casaDoCodigo/app/routes/produtos.js
@@ -2,9 +2,10 @@
 module.exports = function(app) {
     //Rota
     app.get('/produtos', (req, res) => {
-        let connection = app.infra.connectionFactory();
+        let connection = app.infra.connectionFactory()
+        let produtosBanco = new app.infra.ProdutosDAO(connection)
 
-        connection.query('select * from produtos', (err, results) => {
+        produtosBanco.lista((err, results) => {
             // res.send('produtos/lista', {lista:results})
             res.render('produtos/lista', {lista:results})
         })
