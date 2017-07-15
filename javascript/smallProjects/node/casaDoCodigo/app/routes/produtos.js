8436783d13666a8b121d5844c6e058d8847ff793
Geovane Pacheco <geovane.pacheco99@gmail.com>
2017-07-15 17:10:55 -0300

    Criando rota de produtos e realizando conexao com o banco de dados

--- javascript/smallProjects/node/casaDoCodigo/app/routes/produtos.js
+++ javascript/smallProjects/node/casaDoCodigo/app/routes/produtos.js
@@ -2,9 +2,24 @@
 module.exports = function(app) {
     //Rota
     app.get('/produtos', (req, res) => {
-        // res.send('<html><body>Listando os produtos da loja utilizando express!</body></html>')
-        // render(), renderizará uma página
-        res.render('produtos/lista')
+
+        // retorna uma funcao que representa um objeto mysql.
+        let mysql = require('mysql')
+        let connection = mysql.createConnection({
+            host: 'localhost',
+            user: 'geovane',
+            password: '',
+            database: 'casadocodigo'
+        })
+
+        connection.query('select * from produtos', (err, results) => {
+            res.send(results)
+        })
+
+        // fechando a conexao
+        connection.end()
+
+
     })
 
 }
