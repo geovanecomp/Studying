4853fabbad82c7de865e9cb909b0963c117288ad
Geovane Pacheco <geovane.pacheco99@gmail.com>
2017-11-28 23:44:13 -0200

    Setting a max number of attempts

--- ruby/Guess_game/bigger_or_smaller.rb
+++ ruby/Guess_game/bigger_or_smaller.rb
@@ -1,14 +1,28 @@
-number = 111
+secret_number = 111
 puts 'What is your name?'
 name = gets
 puts 'The game will begins ' + name
-puts 'Guess the numer: '
-guess_number = gets.to_i
 
-correct_number = guess_number == number
+max_attempt = 5
 
-if correct_number
-  puts 'You win!'
-else
-  puts 'You lose!'
+for attempt in 1..max_attempt
+  puts attempt.to_s + 'º attempt'
+
+  puts 'Guess the numer: '
+  guess_number = gets.to_i
+
+  correct_number = guess_number == secret_number
+
+  if correct_number
+    puts 'You win!'
+    break
+  else
+    puts 'You lose!'
+    bigger = secret_number > guess_number
+    if bigger
+      puts "The correct number is bigger"
+    else
+      puts "The correct number is smaller"
+    end
+  end
 end
