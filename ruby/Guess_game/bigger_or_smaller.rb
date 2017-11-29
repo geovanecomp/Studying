fa6b459f1deba32e5e60deb633665e7b88f2b96c
Geovane Pacheco <geovane.pacheco99@gmail.com>
2017-11-28 23:15:54 -0200

    Usding ifs to decide the guess number

--- ruby/Guess_game/bigger_or_smaller.rb
+++ ruby/Guess_game/bigger_or_smaller.rb
@@ -1,3 +1,14 @@
+number = 111
 puts 'What is your name?'
 name = gets
 puts 'The game will begins ' + name
+puts 'Guess the numer: '
+guess_number = gets.to_i
+
+correct_number = guess_number == number
+
+if correct_number
+  puts 'You win!'
+else
+  puts 'You lose!'
+end
