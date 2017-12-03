2eb001a859a22ad8b885827a7eafbb60bbd3d0b6
Geovane Pacheco <geovane.pacheco99@gmail.com>
2017-12-03 12:58:46 -0200

    Trying to install via composer

--- devops/vagrant/manifests/web.pp
+++ devops/vagrant/manifests/web.pp
@@ -2,8 +2,38 @@ exec { "apt-update":
   command => "/usr/bin/apt-get update"
 }
 
-package { ["openjdk-8-jre", "tomcat8", "mysql-server"]:
+exec { "createTestDB":
+  command => "mysqladmin -uroot create testDB",
+  unless => "mysql -uroot testDB",
+  path => "/usr/bin",
+  require => Service["mysql"]
+}
+
+exec { "installSlim":
+  command => "composer require slim/slim '^3.0'",
+  path => "/usr/bin",
+  require => Package["composer"]
+}
+
+package { ["openjdk-8-jre", "tomcat8", "mysql-server", "php7.0", "composer"]:
   ensure => installed,
   require => Exec["apt-update"]
 }
+
+service { "tomcat8":
+  ensure => running,
+  enable => true,
+  hasstatus => true,
+  hasrestart => true,
+  require => Package["tomcat8"]
+}
+
+service { "mysql":
+  ensure => running,
+  enable => true,
+  hasstatus => true,
+  hasrestart => true,
+  require => Package ["mysql-server"]
+}
+
 
