e4c9c65178927382399d87cfe110d84578e9c53a
Geovane Pacheco <geovane.pacheco99@gmail.com>
2018-01-17 21:46:10 -0200

    Ajustando responsabilidade de index.js

--- javascript/smallProjects/node/payfast/index.js
+++ javascript/smallProjects/node/payfast/index.js
@@ -1,12 +1,5 @@
-let express = require('express')
-// Carregando a funcao express
-let app = express()
+let app = require('./config/custom-express')()
 
 app.listen(3000, () => {
   console.log('Iniciando servidor na porta 3000');
-})
-
-app.get('/teste', (req, res) => {
-  console.log('Recebendo a conexao de teste');
-  res.send('Recenbendo conexao')
 })
