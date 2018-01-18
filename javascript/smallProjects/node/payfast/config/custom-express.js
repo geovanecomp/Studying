f96ea51c263621baef24d4e6a84fae929b35ca33
Geovane Pacheco <geovane.pacheco99@gmail.com>
2018-01-18 10:03:27 -0200

    Configurando body parser para ligar com o body da requisicao

--- javascript/smallProjects/node/payfast/config/custom-express.js
+++ javascript/smallProjects/node/payfast/config/custom-express.js
@@ -1,9 +1,15 @@
 let express = require('express')
 // Um helper para rotas
 let consign = require('consign')
+let bodyParser = require('body-parser')
 
 module.exports = function () {
   let app = express()
+
+  app.use(bodyParser.urlencoded({extended: true}))
+
+  //  Registrando o body parser na aplicação
+  app.use(bodyParser.json())
 
   // Injetando dentro de app, todas as informações das rotas
   consign()
