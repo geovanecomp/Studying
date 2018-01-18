03cf7b7e767bdc8ca0a7b71885143c96c5e640d0
Geovane Pacheco <geovane.pacheco99@gmail.com>
2018-01-18 13:34:12 -0200

    Metodo para atualizacao de pagamento criado

--- javascript/smallProjects/node/payfast/persistencia/pagamentoDao.js
+++ javascript/smallProjects/node/payfast/persistencia/pagamentoDao.js
@@ -6,6 +6,12 @@ pagamentoDao.prototype.salvar = function(pagamento,callback) {
     this._connection.query('INSERT INTO pagamentos SET ?', pagamento, callback);
 }
 
+pagamentoDao.prototype.atualizar = function(pagamento,callback) {
+    console.log('Os dados na att sao: ', pagamento);
+    this._connection.query('UPDATE pagamentos SET ? WHERE id = ?', [pagamento, pagamento.id], callback);
+}
+
+
 pagamentoDao.prototype.listar = function(callback) {
     this._connection.query('select * from pagamentos',callback);
 }
