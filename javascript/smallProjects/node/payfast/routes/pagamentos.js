cb5831774a2650d703a4ef10b4ce987781df07ae
Geovane Pacheco <geovane.pacheco99@gmail.com>
2018-01-18 13:34:23 -0200

    Rota para atualizacao de pagamento criada

--- javascript/smallProjects/node/payfast/routes/pagamentos.js
+++ javascript/smallProjects/node/payfast/routes/pagamentos.js
@@ -3,6 +3,39 @@ module.exports = function(app) {
     res.send('Exibindo rota de pagamentos')
   })
 
+  app.put('/pagamentos/pagamento/:id', (req, res) => {
+    req.assert('forma_de_pagamento',
+      'Forma de pagamento é de preenchimento obrigatório'). notEmpty()
+
+    req.assert('valor',
+      'Valor é de preenchimento obrigatório e deve ser um decimal')
+        .notEmpty().isFloat()
+
+    let requestErrors = req.validationErrors()
+
+    if (requestErrors) {
+      console.log('Foram encontrados erros de validação');
+      res.status(400).send(requestErrors)
+      return
+    }
+
+    let pagamento = req.body
+    pagamento.id = req.params.id
+    pagamento.data = new Date
+
+    let connection = app.persistencia.connectionFactory()
+    let pagamentoDao = new app.persistencia.pagamentoDao(connection)
+
+    pagamentoDao.atualizar(pagamento, (erro, resultado) => {
+      if (erro) {
+        res.status(500).send(erro)
+      } else {
+        res.status(200).json(pagamento)
+      }
+    })  
+
+  })
+
   app.post('/pagamentos/pagamento', (req, res) => {
     req.assert('forma_de_pagamento',
      'Forma de pagamento é de preenchimento obrigatório').notEmpty()
@@ -28,7 +61,7 @@ module.exports = function(app) {
 
     pagamentoDao.salvar(pagamento, (erro, resultado) => {
       if (erro) {
-        res.status(400).send(erro)
+        res.status(500).send(erro)
       } else {
         res.location('/pagamentos/pagamento/' + resultado.insertId)
         console.log('Pagamento criado')
