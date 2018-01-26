f3f0297ed1d2dab45d39a9249043bff9cde17b63
Geovane Pacheco <geovane.pacheco99@gmail.com>
2018-01-26 13:05:54 -0200

    Componentizando elementos de autor

--- javascript/smallProjects/react/cdc-admin/src/App.js
+++ javascript/smallProjects/react/cdc-admin/src/App.js
@@ -3,147 +3,34 @@ import './css/pure-min.css';
 import './css/side-menu.css';
 import $ from 'jquery';
 import InputCustomizado from './components/InputCustomizado'
-
+import {FormularioAutor, TabelaAutores} from './Autor'
 class App extends Component {
-
-  constructor() {
-    super();
-    // Setando o escopo do react para a funcao
-    this.submeterForm = this.submeterForm.bind(this)
-    this.setNome = this.setNome.bind(this)
-    this.setEmail = this.setEmail.bind(this)
-    this.setSenha = this.setSenha.bind(this)
-    // Propriedade state eh diponibilizada pelo proprio react
-    this.state = {
-      lista : [],
-      nome: '',
-      email: '',
-      senha: ''
-      // form: {
-      //   nome: '',
-      //   email: '',
-      //   senha: ''
-      // }
-    };
-  }
-
-  setNome(evento) {
-    this.setState({nome: evento.target.value})
-    // this.setState({form: {nome: evento.target.value}})
-  }
-
-  setEmail(evento) {
-    this.setState({email: evento.target.value})
-    // this.setState({form: {email: evento.target.value}})
-  }
-
-  setSenha(evento) {
-    this.setState({senha: evento.target.value})
-    // this.setState({form: {senha: evento.target.value}})
-  }
-
-  // Hook disparado logo apos montar o componente
-  componentDidMount(){
-    $.ajax({
-        // Servidor remoto onde se encontra o backend
-        url:"http://cdc-react.herokuapp.com/api/autores",
-        dataType: 'json',
-        success:function(resposta){
-          // Para atualizar a propriedade dinamicamente
-          this.setState({lista:resposta});
-        }.bind(this) // Setando o scopo para o react inves do jquery
-      }
-    );
-  }
-
-  submeterForm(evento) {
-    // Para nao atualizar a pagina
-    evento.preventDefault()
-
-    let state = this.state
-    $.ajax({
-      url:"http://cdc-react.herokuapp.com/api/autores",
-      contentType: 'application/json',
-      dataType: 'json',
-      type: 'post',
-      data: JSON.stringify({nome: state.nome, email: state.email, senha: state.senha}),
-      success: (resposta) => {
-        this.setState({lista: resposta})
-      },
-      error: (error) => {
-        console.log('Enviado com erro');
-      }
-    })
-  }
 
   render() {
     return (
       <div id="layout">
-
-          <a href="#menu" id="menuLink" className="menu-link">
-
-              <span></span>
-          </a>
-
-          <div id="menu">
-              <div className="pure-menu">
-                  <a className="pure-menu-heading" href="#">Company</a>
-
-                  <ul className="pure-menu-list">
-                      <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
-                      <li className="pure-menu-item"><a href="#" className="pure-menu-link">Autor</a></li>
-                      <li className="pure-menu-item"><a href="#" className="pure-menu-link">Livro</a></li>
-
-
-                  </ul>
-              </div>
+        <a href="#menu" id="menuLink" className="menu-link">
+          <span></span>
+        </a>
+        <div id="menu">
+          <div className="pure-menu">
+            <a className="pure-menu-heading" href="#">Company</a>
+            <ul className="pure-menu-list">
+              <li className="pure-menu-item"><a href="#" className="pure-menu-link">Home</a></li>
+              <li className="pure-menu-item"><a href="#" className="pure-menu-link">Autor</a></li>
+              <li className="pure-menu-item"><a href="#" className="pure-menu-link">Livro</a></li>
+            </ul>
           </div>
-
-              <div id="main">
-                  <div className="header">
-                    <h1>Cadastro de Autores</h1>
-                  </div>
-                  <div className="content" id="content">
-                    <div className="pure-form pure-form-aligned">
-                      <form className="pure-form pure-form-aligned" onSubmit={this.submeterForm} >
-                        <InputCustomizado id="nome" label="Nome" type="text" value={this.state.nome} onChange={this.setNome}></InputCustomizado>
-                        <InputCustomizado id="email" label="Email" type="email" value={this.state.email} onChange={this.setEmail}></InputCustomizado>
-                        <InputCustomizado id="senha" label="Senha" type="password" value={this.state.senha} onChange={this.setSenha}></InputCustomizado>
-                        
-                        <div className="pure-control-group">
-                          <label></label>
-                          <button type="submit" className="pure-button pure-button-primary">Gravar</button>
-                        </div>
-                      </form>
-
-                    </div>
-                    <div>
-                      <table className="pure-table">
-                        <thead>
-                          <tr>
-                            <th>Nome</th>
-                            <th>email</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          {
-                            this.state.lista.map(function(autor){
-                              return (
-                                // A fim de otimizar a performance, deve-se setar um identificador em cada elemento
-                                <tr key={autor.id}>
-                                  <td>{autor.nome}</td>
-                                  <td>{autor.email}</td>
-                                </tr>
-                              );
-                            })
-                          }
-                        </tbody>
-                      </table>
-                    </div>
-                  </div>
-                </div>
-
-
+        </div>
+        <div id="main">
+          <div className="header">
+            <h1>Cadastro de Autores</h1>
+          </div>
+          <div className="content" id="content">
+            <FormularioAutor/>
+            <TabelaAutores/>
+          </div>
+        </div>
       </div>
     );
   }
---
+++ javascript/smallProjects/react/cdc-admin/src/Autor.js
@@ -0,0 +1,124 @@
+import React, { Component } from 'react'
+import $ from 'jquery'
+import InputCustomizado from './components/InputCustomizado'
+
+export class FormularioAutor extends Component{
+  constructor() {
+    super();
+    // Setando o escopo do react para a funcao
+    this.submeterForm = this.submeterForm.bind(this)
+    this.setNome = this.setNome.bind(this)
+    this.setEmail = this.setEmail.bind(this)
+    this.setSenha = this.setSenha.bind(this)
+    // Propriedade state eh diponibilizada pelo proprio react
+    this.state = {
+      nome: '',
+      email: '',
+      senha: ''
+    };
+  }
+  // Para retornar o componente para ser renderizado na view
+  render() {
+    return(
+      <div className="pure-form pure-form-aligned">
+        <form className="pure-form pure-form-aligned" onSubmit={this.submeterForm} >
+          <InputCustomizado id="nome" label="Nome" type="text" value={this.state.nome} onChange={this.setNome}></InputCustomizado>
+          <InputCustomizado id="email" label="Email" type="email" value={this.state.email} onChange={this.setEmail}></InputCustomizado>
+          <InputCustomizado id="senha" label="Senha" type="password" value={this.state.senha} onChange={this.setSenha}></InputCustomizado>
+
+          <div className="pure-control-group">
+            <label></label>
+            <button type="submit" className="pure-button pure-button-primary">Gravar</button>
+          </div>
+        </form>
+
+      </div>
+    )
+  }
+
+  submeterForm(evento) {
+    // Para nao atualizar a pagina
+    evento.preventDefault()
+
+    let state = this.state
+    $.ajax({
+      url:"http://cdc-react.herokuapp.com/api/autores",
+      contentType: 'application/json',
+      dataType: 'json',
+      type: 'post',
+      data: JSON.stringify({nome: state.nome, email: state.email, senha: state.senha}),
+      success: (resposta) => {
+        this.setState({lista: resposta})
+      },
+      error: (error) => {
+        console.log('Enviado com erro');
+      }
+    })
+  }
+
+  setNome(evento) {
+    this.setState({nome: evento.target.value})
+    // this.setState({form: {nome: evento.target.value}})
+  }
+
+  setEmail(evento) {
+    this.setState({email: evento.target.value})
+    // this.setState({form: {email: evento.target.value}})
+  }
+
+  setSenha(evento) {
+    this.setState({senha: evento.target.value})
+    // this.setState({form: {senha: evento.target.value}})
+  }
+}
+
+export class TabelaAutores extends Component {
+
+  constructor() {
+    super();
+    // Propriedade state eh diponibilizada pelo proprio react
+    this.state = {
+      lista : [],
+    };
+  }
+
+  // Hook disparado logo apos montar o componente
+  componentDidMount(){
+    $.ajax({
+        // Servidor remoto onde se encontra o backend
+        url:"http://cdc-react.herokuapp.com/api/autores",
+        dataType: 'json',
+        success:function(resposta){
+          // Para atualizar a propriedade dinamicamente
+          this.setState({lista:resposta});
+        }.bind(this) // Setando o scopo para o react inves do jquery
+      }
+    );
+  }
+
+  render() {
+    return (
+      <table className="pure-table">
+        <thead>
+          <tr>
+            <th>Nome</th>
+            <th>email</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            this.state.lista.map(function(autor){
+              return (
+                // A fim de otimizar a performance, deve-se setar um identificador em cada elemento
+                <tr key={autor.id}>
+                  <td>{autor.nome}</td>
+                  <td>{autor.email}</td>
+                </tr>
+              );
+            })
+          }
+        </tbody>
+      </table>
+    )
+  }
+}
