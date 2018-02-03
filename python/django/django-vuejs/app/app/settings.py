606e6d4630fd22340dc95fd9f935528370ee9088
Geovane Pacheco <geovane.pacheco99@gmail.com>
2018-02-03 21:39:03 -0200

    Setting database and static files configuration

--- python/django/django-vuejs/app/app/settings.py
+++ python/django/django-vuejs/app/app/settings.py
@@ -75,10 +75,19 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
+
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+         'NAME': 'britecore',
+         'ENGINE': 'django.db.backends.mysql',
+         'USER': 'geovane',
+         'PASSWORD': '',
+         'HOST': 'localhost',
+         'PORT': '3306'
+     }
 }
 
 
@@ -119,3 +128,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
