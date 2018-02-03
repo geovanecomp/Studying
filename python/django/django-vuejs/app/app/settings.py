78ab9a914396eb0d251b625ffb18b70aeb3d9bc4
Geovane Pacheco <geovane.pacheco99@gmail.com>
2018-02-03 21:49:38 -0200

    Setting templates configuration

--- python/django/django-vuejs/app/app/settings.py
+++ python/django/django-vuejs/app/app/settings.py
@@ -55,7 +55,9 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            '{}/templates'.format(BASE_DIR)
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
