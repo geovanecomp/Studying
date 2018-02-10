36d8b70747b1d24e3820a3bf73f084be2e0f491a
Geovane Pacheco <geovane.pacheco99@gmail.com>
2018-02-10 10:52:24 -0200

    Creating a simple automated test

--- python/django/django-vuejs/django-vuejs/homepage/tests.py
+++ python/django/django-vuejs/django-vuejs/homepage/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
-from .views import
-# Create your tests here.
+from .models import Games
+
+class GamesTestCase(TestCase):
+    def setUp(self):
+        Games.objects.create(titulo='test_title', plataformas='test_plat')
+        Games.objects.create(titulo='test_title2', plataformas='test_plat2')
+
+    def test_games_should_have_title(self):
+        """All games must have the title information"""
+
+        game1 = Games.objects.get(titulo='test_title')
+        self.hasattr(game1, 'titulo')
