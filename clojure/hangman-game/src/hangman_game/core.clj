3752606aedf3f3bcf9779be8870e8192eb0b039b
Geovane Pacheco <geovane.pacheco99@gmail.com>
2018-03-17 20:04:44 -0300

    Smart recursive call implemented

--- clojure/hangman-game/src/hangman_game/core.clj
+++ clojure/hangman-game/src/hangman_game/core.clj
@@ -21,24 +21,28 @@
   (.contains word, user-guess)
 )
 
-(defn analyse-user-guess [user-guess lives word hits]
-  (if (correct? user-guess word)
-    (game lives word (conj hits user-guess))
-    (game (dec lives) word hits)
-  )
-)
-
 ; Normaly for boolean functions we should use the '?'
 (defn correct-word? [word hits]
   (empty? (missing-letters word hits))
 )
 
 (defn game [lives word hits]
-  (if (= lives 0)
-    (lose)
-    (if (correct-word? word hits)
-      (win)
-      (analyse-user-guess (read-letter!) lives word hits)
+  (cond
+    (= lives 0) (lose)
+    (correct-word? word hits) (win)
+    :else
+    ( let [user-guess (read-letter!)]
+      (if (correct? user-guess word)
+        (do
+          (println "Correct letter!")
+          ; To do a smart recursive call, it's necessary to use recur instead the function name
+          (recur lives word (conj hits user-guess))
+        )
+        (do
+          (println "Incorrect letter!")
+          (recur (dec lives) word hits)
+        )
+      )
     )
   )
 )
