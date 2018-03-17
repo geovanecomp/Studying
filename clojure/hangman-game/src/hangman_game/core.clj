c5ffa042901af01cd1ae5373fcce43b6f3d96fb4
Geovane Pacheco <geovane.pacheco99@gmail.com>
2018-03-17 12:56:50 -0300

    Setting the life counter

--- clojure/hangman-game/src/hangman_game/core.clj
+++ clojure/hangman-game/src/hangman_game/core.clj
@@ -1,6 +1,20 @@
 (ns hangman-game.core
   (:gen-class))
 
+(def number-of-lives 6)
+
+(defn lose [] (print "You lose"))
+
+(defn game [lives]
+  (if (= lives 0)
+    (lose)
+    (do
+        (print lives)
+        (game (dec lives))
+    )
+  )
+)
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
