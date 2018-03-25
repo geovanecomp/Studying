8442d4c1ea841f8e266bf7994cd055d18fa7df71
Geovane Pacheco <geovane.pacheco99@gmail.com>
2018-03-25 17:41:40 -0300

    Creating a helper file to auxiliary functions

--- ruby/nasa_challenge/app/helper.rb
+++ ruby/nasa_challenge/app/helper.rb
@@ -1,11 +1,3 @@
-# For matrix, the first position is the top left corner instead the bottom left.
-def adjust_cordinate_y (probe_position_y, plateau_len_y)
-  # Matrix is from 0 until len_y
-  plateau_len_y -= plateau_len_y
-
-  return plateau_len_y - probe_position_y
-end
-
 def is_valid_position (probe_position_x, probe_position_y, plateau_len_x, plateau_len_y)
   if probe_position_x > plateau_len_x or probe_position_y > plateau_len_y
     puts("The informed position is bigger than plateau size")
