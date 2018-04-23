bed28f13487475271c121773ac9f6966df70175f
Geovane Pacheco <geovane.pacheco99@gmail.com>
2018-04-23 12:35:09 -0300

    Utilizando react-router

--- javascript/smallProjects/react/cdc-admin/src/index.js
+++ javascript/smallProjects/react/cdc-admin/src/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import AutorBox from './Autor';
 import './index.css';
+import {BrowserRouter, Route} from 'react-router-dom';
+
 
 ReactDOM.render(
-  <App />,
+  <BrowserRouter>
+    <div>
+      <Route path='/' component={App} />
+      <Route path='/autor' component={AutorBox} />
+      <Route path='/livro' />
+    </div>
+  </BrowserRouter>,
   document.getElementById('root')
 );
